c39204f005c5589c5efd941208cfd271cc9ea882
lsh324 <83631827+lsh324@users.noreply.github.com>
2021-09-10 21:22:31 +0900

    Delete momentum directory

--- momentum/script/promise.js
+++
@@ -1,7 +0,0 @@
-const url = 'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-
-fetch(url).then(response=>{
-  return response.json();
-}).then( (data)=>{
-  console.log( data );
-});
