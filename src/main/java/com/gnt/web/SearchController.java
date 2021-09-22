9a0aa218375d165f4ae0722ed723c8926f203d30
kyh119 <dydgus.k@gmail.com>
2021-09-22 10:37:23 +0900

    url mapping

--- src/main/java/com/gnt/web/SearchController.java
+++ src/main/java/com/gnt/web/SearchController.java
@@ -2,12 +2,19 @@ package com.gnt.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class SearchController {
 
-    @GetMapping("/search/{summonerName}")
-    public String search() {
+    @GetMapping("/search")
+    public String search(@RequestParam(value = "userName") String userName) {
+        return "redirect:/search/" + userName;
+    }
+
+    @GetMapping("/search/{userName}")
+    public String searchByName(@PathVariable String userName) {
         return "summoner";
     }
 
