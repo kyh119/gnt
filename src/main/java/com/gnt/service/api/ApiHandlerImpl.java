0ddaf09ff3aa02e1aae353af923529518324b4a4
kyh119 <dydgus.k@gmail.com>
2021-10-15 19:14:26 +0900

    gitignore 수정

--- src/main/java/com/gnt/service/api/ApiHandlerImpl.java
+++ src/main/java/com/gnt/service/api/ApiHandlerImpl.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-@PropertySource("classpath:/application-api-key.properties")
 @Component
 public class ApiHandlerImpl implements ApiHandler {
 
